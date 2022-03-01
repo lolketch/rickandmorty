a886d143d8587d6aabc16efe61d3e1cec96c507e
lolketch <lightning110299@gmail.com>
2022-03-01 11:40:01 +1000

    clean structure was added in feature list module

--- character_list/src/main/java/com/example/character_list/presentation/CharacterListFragment.kt
+++ character_list/src/main/java/com/example/character_list/presentation/CharacterListFragment.kt
@@ -1,6 +1,7 @@
-package com.example.character_list
+package com.example.character_list.presentation
 
 import androidx.fragment.app.Fragment
+import com.example.character_list.R
 
 class CharacterListFragment: Fragment(R.layout.fragment_character_list) {
 }
